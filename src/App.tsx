5c48c1215aa1beef7a56e18d2f73e2e15b1b3e9d
ChrisEgan06 <eganchristopher06@gmail.com>
2026-08-29 23:40:57 -0400

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Experience from "./pages/experience";
 import Hobbies from "./pages/hobbies";
 import ProjectDetail from "./pages/project-detail";
 import Navbar from "./components/navbar";
+import { Analytics } from "@vercel/analytics/next"
 
 /**
  * Main App component
@@ -14,6 +15,7 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
